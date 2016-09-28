81c09279b88ca6ab3daeaa62059f697bcf3b7329
Diane Hoffstetter <dhoffstetter@mac.com>
2016-09-27 21:10:48 -0700

    Started to add user picker

--- HunnyDue/AddHunnyDueItemViewController.swift
+++ HunnyDue/AddHunnyDueItemViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class AddHunnyDueItemViewController: UIViewController {
+class AddHunnyDueItemViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
   @IBOutlet weak var shortDescriptionTextField: UITextField!
   @IBOutlet weak var assignToTextField: UITextField!
+  @IBOutlet weak var assignToPicker: UIPickerView!
   @IBOutlet weak var dueDatePicker: UIDatePicker!
   
-  
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  var users: [User] = []
 
-        // Do any additional setup after loading the view.
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+      
+      // Configure datePicker
+      
+      dueDatePicker.minimumDate = Date(timeIntervalSinceNow: 60*10)
+      
+      // Configure the AssignTo picker
+      
+      self.assignToPicker.delegate = self
+      self.assignToPicker.dataSource = self
+      
+      // Use dummy values for testing
+      
+      let user1 = User()
+      user1.email = "diane@dbx.com"
+      user1.name = "Diane"
+      users.append(user1)
+      
+      let user2 = User()
+      user2.email = "dave@dbx.com"
+      user2.name = "Dave"
+      users.append(user2)
+      
+      let user3 = User()
+      user3.email = "amanda@dbx.com"
+      user3.name = "Amanda"
+      users.append(user3)
+
+      
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +59,19 @@ class AddHunnyDueItemViewController: UIViewController {
 
   @IBAction func assignItButtonPressed(_ sender: AnyObject) {
   }
-
+  
+  // UIPicker
+  
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return users.count
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return users[row].name
+  }
 
 }
---
+++ HunnyDue/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  HunnyDue
+//
+//  Created by Diane Hoffstetter on 9/27/16.
+//  Copyright © 2016 Dumb Blonde Software. All rights reserved.
+//
+
+import Foundation
+
+class User {
+  
+  var email = ""
+  var name = ""
+}
